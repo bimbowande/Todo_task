604e90f07ae965d1293b003973c0c899c62d9551
bimbowande <bimbowande@gmail.com>
2024-09-30 00:23:43 +0100

    setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Todo_task",
   resolve: {
     alias: {
       "@": "/src", // This sets the "@" alias to your source directory.
